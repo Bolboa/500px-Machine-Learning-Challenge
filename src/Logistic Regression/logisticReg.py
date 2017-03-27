add85b351933ff0de39c99de8c7512bf3f249dcc
Eric Bolboa <ebolboa@gmail.com>
2017-03-26 22:44:35 -0400

    gradient descent implemented to generate adversarial image

--- src/Logistic Regression/logisticReg.py
+++ src/Logistic Regression/logisticReg.py
@@ -11,7 +11,7 @@ flags = tf.app.flags
 FLAGS = flags.FLAGS
 flags.DEFINE_float('learning_rate', 0.01, 'Initial learning rate.')
 flags.DEFINE_integer('training_epochs', 25, 'Number of times training vectors are used once to update weights.')
-flags.DEFINE_integer('batch_size', 100, 'Batch size. Must divide evenly into the data set sizes.')
+flags.DEFINE_integer('batch_size', 1, 'Batch size. Must divide evenly into the data set sizes.')
 flags.DEFINE_integer('display_step', 1, 'Tells function to print out progress after every epoch')
 
 def logistic_regression():
@@ -19,39 +19,35 @@ def logistic_regression():
     mnist = input_data.read_data_sets("/tmp/data/", one_hot=True)
 
     # tf Graph Input
-    x = tf.placeholder(tf.float32, [None, 784])  # mnist data image of shape 28*28=784
-    y = tf.placeholder(tf.float32, [None, 10])  # 0-9 digits recognition => 10 classes
+    x = tf.get_variable("input_image", shape=[1,784], dtype=tf.float32)
+    x_placeholder = tf.placeholder(tf.float32, shape=[1, 784])
+    assign_x_op = x.assign(x_placeholder).op
+    
+    y = tf.placeholder(shape=[1,10], name='input_label', dtype=tf.float32)  # 0-9 digits recognition => 10 classes
+
 
     # set model weights
-    W = tf.Variable(tf.zeros([784, 10]))
-    b = tf.Variable(tf.zeros([10]))
+    W = tf.get_variable("weights", shape=[784, 10], dtype=tf.float32, initializer=tf.random_normal_initializer())
+    b = tf.get_variable("biases", shape=[1, 10], dtype=tf.float32, initializer=tf.zeros_initializer())
 
     # construct model
     logits = tf.matmul(x, W) + b
     pred = tf.nn.softmax(logits)  # Softmax
-
+    
     # minimize error using cross entropy
     cost = tf.reduce_mean(-tf.reduce_sum(y * tf.log(pred), reduction_indices=1))
     # Gradient Descent
     optimizer = tf.train.GradientDescentOptimizer(FLAGS.learning_rate).minimize(cost)
 
     # initializing the variables
-    init = tf.initialize_all_variables()
+    init = tf.global_variables_initializer()
 
     saver = tf.train.Saver()
-
+    
     # launch the graph
     with tf.Session() as sess:
-        #mnist.test.images[0] += 0.5*weight
 
         sess.run(init)
-
-        # extract first image and its corresponding label
-        first_image = mnist.test.images[0] * 255
-        first_label = mnist.test.labels[0]
-
-        # reshape into a 28x28 image
-        pixels = first_image.reshape((28, 28))
 
         # training cycle
         for epoch in range(FLAGS.training_epochs):
@@ -60,9 +56,10 @@ def logistic_regression():
             # loop over all batches
             for i in range(total_batch):
                 batch_xs, batch_ys = mnist.train.next_batch(FLAGS.batch_size)
-
-                _, c = sess.run([optimizer, cost], feed_dict={x: batch_xs, y: batch_ys})
-
+                # Assign the contents of `batch_xs` to variable `x`.
+                sess.run(assign_x_op, feed_dict={x_placeholder: batch_xs})
+                _, c = sess.run([optimizer, cost], feed_dict={y: batch_ys})
+                
                 # compute average loss
                 avg_cost += c / total_batch
             # display logs per epoch step
@@ -74,25 +71,24 @@ def logistic_regression():
         print("Optimization Finished!")
 
         # list of booleans to determine the correct predictions
-        correct_prediction = tf.equal(tf.argmax(pred, 1), tf.argmax(y, 1))
-        print(correct_prediction.eval({x:mnist.test.images, y:mnist.test.labels}))
+        #correct_prediction = tf.equal(tf.argmax(pred, 1), tf.argmax(y, 1))
+        #print(correct_prediction.eval({x_placeholder:mnist.test.images, y:mnist.test.labels}))
 
         # calculate total accuracy
-        accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-        print("Accuracy:", accuracy.eval({x: mnist.test.images, y: mnist.test.labels}))
+        #accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+        #print("Accuracy:", accuracy.eval({x: mnist.test.images, y: mnist.test.labels}))
 
         # display predicted label
-        learned_label = sess.run(y, {x: mnist.test.images, y: mnist.test.labels})[0]
-        learned_image = sess.run(x, {x: mnist.test.images, y: mnist.test.labels})[0]
-        learned_image = learned_image.reshape((28,28))
-        og_weights = sess.run(W[:,0])
+        #learned_label = sess.run(y, {x: mnist.test.images, y: mnist.test.labels})[0]
+        #learned_image = sess.run(x, {x: mnist.test.images, y: mnist.test.labels})[0]
+        #learned_image = learned_image.reshape((28,28))
+        #og_weights = sess.run(W[:,0])
 
         # title of image will be the predicted label
-        predicted_label = learned_label.tolist().index(1)
-        plt.suptitle(predicted_label)
-        plt.imshow(learned_image, cmap='gray')
-        plt.show()
-
+        #predicted_label = learned_label.tolist().index(1)
+        #plt.suptitle(predicted_label)
+        #plt.imshow(learned_image, cmap='gray')
+        #plt.show()
 
 
 def main(_):
--- src/Logistic Regression/model.py
+++ src/Logistic Regression/model.py
@@ -7,7 +7,7 @@ flags = tf.app.flags
 FLAGS = flags.FLAGS
 flags.DEFINE_float('learning_rate', 0.01, 'Initial learning rate.')
 flags.DEFINE_integer('training_epochs', 25, 'Number of times training vectors are used once to update weights.')
-flags.DEFINE_integer('batch_size', 100, 'Batch size. Must divide evenly into the data set sizes.')
+flags.DEFINE_integer('batch_size', 1, 'Batch size. Must divide evenly into the data set sizes.')
 flags.DEFINE_integer('display_step', 1, 'Tells function to print out progress after every epoch')
 
 
@@ -16,12 +16,15 @@ def use_model():
     mnist = input_data.read_data_sets("/tmp/data/", one_hot=True)
 
     # tf Graph Input
-    x = tf.placeholder(tf.float32, [None, 784])  # mnist data image of shape 28*28=784
-    y = tf.placeholder(tf.float32, [None, 10])  # 0-9 digits recognition => 10 classes
+    x = tf.get_variable("input_image", shape=[1,784], dtype=tf.float32)
+    #x_placeholder = tf.placeholder(tf.float32, shape=[1, 784])
+    #assign_x_op = x.assign(x_placeholder).op
+    
+    y = tf.placeholder(shape=[1,10], name='input_label', dtype=tf.float32)  # 0-9 digits recognition => 10 classes
 
     # set model weights
-    W = tf.Variable(tf.zeros([784, 10]))
-    b = tf.Variable(tf.zeros([10]))
+    W = tf.get_variable("weights", shape=[784, 10], dtype=tf.float32, initializer=tf.random_normal_initializer())
+    b = tf.get_variable("biases", shape=[1, 10], dtype=tf.float32, initializer=tf.zeros_initializer())
 
     # construct model
     logits = tf.matmul(x, W) + b
@@ -33,7 +36,7 @@ def use_model():
     optimizer = tf.train.GradientDescentOptimizer(FLAGS.learning_rate).minimize(cost)
 
     # initializing the variables    
-    init = tf.initialize_all_variables()
+    init = tf.global_variables_initializer()
 
     saver = tf.train.Saver()
 
@@ -41,12 +44,22 @@ def use_model():
         saver.restore(sess, "/tmp/model.ckpt")
         
         sample_image, sample_label = mnist.test.next_batch(1)
-        sess.run(pred, feed_dict={x:sample_image})
-
+        #print(sess.run(x))
+        #print(x.eval())
+        sess.run(tf.assign(x, sample_image))
+        sess.run(pred)
+    
+        #print(sess.run(x))
+        #print(x.eval())
+        
         fake_label = tf.placeholder(tf.int32, shape=[1])
         fake_loss = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=logits,labels=fake_label)
 
-        adversarial_step = tf.train.GradientDescentOptimizer(learning_rate=1e-3).minimize(fake_loss, var_list=[sample_image])
+        adversarial_step = tf.train.GradientDescentOptimizer(learning_rate=1e-3).minimize(fake_loss, var_list=[x])
+
+        sess.run(adversarial_step, feed_dict={fake_label:np.array([8])})
+        plt.imshow(sess.run(x).reshape(28,28),cmap='gray')
+        sess.run(pred)
         
         #x_in = np.expand_dims(mnist.test.images[0], axis=0)
         #y_in = np.expand_dims(mnist.test.labels[0], axis=0)
