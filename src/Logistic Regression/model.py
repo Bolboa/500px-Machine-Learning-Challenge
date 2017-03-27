7dff695875a6e616814813e62b505e69aa827ef4
Eric Bolboa <ebolboa@gmail.com>
2017-03-27 00:25:34 -0400

    generate successful adversarial image

--- src/Logistic Regression/model.py
+++ src/Logistic Regression/model.py
@@ -17,9 +17,6 @@ def use_model():
 
     # tf Graph Input
     x = tf.get_variable("input_image", shape=[1,784], dtype=tf.float32)
-    #x_placeholder = tf.placeholder(tf.float32, shape=[1, 784])
-    #assign_x_op = x.assign(x_placeholder).op
-    
     y = tf.placeholder(shape=[1,10], name='input_label', dtype=tf.float32)  # 0-9 digits recognition => 10 classes
 
     # set model weights
@@ -41,35 +38,33 @@ def use_model():
     saver = tf.train.Saver()
 
     with tf.Session() as sess:
+        # reload model
         saver.restore(sess, "/tmp/model.ckpt")
-        
+
+        # get number 7 from mnist dataset
         sample_image, sample_label = mnist.test.next_batch(1)
-        #print(sess.run(x))
-        #print(x.eval())
+
+        # assign the sample image to the variable
         sess.run(tf.assign(x, sample_image))
+        # setup softmax
         sess.run(pred)
-    
-        #print(sess.run(x))
-        #print(x.eval())
-        
+
+        # placeholder for target label
         fake_label = tf.placeholder(tf.int32, shape=[1])
+        # minimize the cross entropy using the loss calculated using the target value 
         fake_loss = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=logits,labels=fake_label)
 
         adversarial_step = tf.train.GradientDescentOptimizer(learning_rate=1e-3).minimize(fake_loss, var_list=[x])
-
-        sess.run(adversarial_step, feed_dict={fake_label:np.array([8])})
-        plt.imshow(sess.run(x).reshape(28,28),cmap='gray')
-        sess.run(pred)
         
-        #x_in = np.expand_dims(mnist.test.images[0], axis=0)
-        #y_in = np.expand_dims(mnist.test.labels[0], axis=0)
-        #classification = sess.run(tf.argmax(pred, 1), feed_dict={x:x_in})
-        #print(classification)
-
-        #accuracy = tf.reduce_mean(tf.cast(classification, tf.float32))
-        #print("Accuracy:", accuracy.eval({x: x_in , y: y_in}))
-
-
+        for i in range(100):
+            sess.run(adversarial_step, feed_dict={fake_label:np.array([8])})
+            sess.run(pred)
+        plt.imshow(sess.run(x).reshape(28,28),cmap='gray')
+        plt.show()
+        
+        x_in = np.expand_dims(x[0], axis=0)
+        classification = sess.run(tf.argmax(pred, 1))
+        print(classification)
 
 use_model()
 
