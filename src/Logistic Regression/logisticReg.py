98772dd5f79a7f0948c6a1fe8dd8412fb50f81e1
Eric Bolboa <ebolboa@gmail.com>
2017-04-22 00:53:55 -0400

    played around with different epsilon values to see if a correct prediction can be made

--- src/Logistic Regression/logisticReg.py
+++ src/Logistic Regression/logisticReg.py
@@ -11,7 +11,7 @@ flags = tf.app.flags
 FLAGS = flags.FLAGS
 flags.DEFINE_float('learning_rate', 0.01, 'Initial learning rate.')
 flags.DEFINE_integer('training_epochs', 25, 'Number of times training vectors are used once to update weights.')
-flags.DEFINE_integer('batch_size', 10, 'Batch size. Must divide evenly into the data set sizes.')
+flags.DEFINE_integer('batch_size', 100, 'Batch size. Must divide evenly into the data set sizes.')
 flags.DEFINE_integer('display_step', 1, 'Tells function to print out progress after every epoch')
 
 def logistic_regression():
@@ -19,11 +19,11 @@ def logistic_regression():
     mnist = input_data.read_data_sets("/tmp/data/", one_hot=True)
 
     # tf Graph Input
-    x = tf.get_variable("input_image", shape=[10,784], dtype=tf.float32)
-    x_placeholder = tf.placeholder(tf.float32, shape=[10, 784])
+    x = tf.get_variable("input_image", shape=[100,784], dtype=tf.float32)
+    x_placeholder = tf.placeholder(tf.float32, shape=[100, 784])
     assign_x_op = x.assign(x_placeholder).op
     
-    y = tf.placeholder(shape=[10,10], name='input_label', dtype=tf.float32)  # 0-9 digits recognition => 10 classes
+    y = tf.placeholder(shape=[100,10], name='input_label', dtype=tf.float32)  # 0-9 digits recognition => 10 classes
 
 
     # set model weights
@@ -36,6 +36,7 @@ def logistic_regression():
     
     # minimize error using cross entropy
     cost = tf.reduce_mean(-tf.reduce_sum(y * tf.log(pred), reduction_indices=1))
+    
     # Gradient Descent
     optimizer = tf.train.GradientDescentOptimizer(FLAGS.learning_rate).minimize(cost)
 
@@ -71,8 +72,8 @@ def logistic_regression():
         print("Optimization Finished!")
 
         # list of booleans to determine the correct predictions
-        #correct_prediction = tf.equal(tf.argmax(pred, 1), tf.argmax(y, 1))
-        #print(correct_prediction.eval({x_placeholder:mnist.test.images, y:mnist.test.labels}))
+        correct_prediction = tf.equal(tf.argmax(pred, 1), tf.argmax(y, 1))
+        print(correct_prediction.eval({x_placeholder:mnist.test.images, y:mnist.test.labels}))
 
         # calculate total accuracy
         #accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
@@ -94,5 +95,4 @@ def logistic_regression():
 def main(_):
     logistic_regression()
 if __name__ == '__main__':
-  tf.app.run()
-
+    tf.app.run()
--- src/Logistic Regression/model.py
+++ src/Logistic Regression/model.py
@@ -6,7 +6,7 @@ from tensorflow.examples.tutorials.mnist import input_data
 flags = tf.app.flags
 FLAGS = flags.FLAGS
 flags.DEFINE_float('learning_rate', 1e-3, 'Initial learning rate.')
-flags.DEFINE_integer('training_epochs', 30, 'Number of times training vectors are used once to update weights.')
+flags.DEFINE_integer('training_epochs', 20, 'Number of times training vectors are used once to update weights.')
 flags.DEFINE_integer('batch_size', 1, 'Batch size. Must divide evenly into the data set sizes.')
 
 def use_model():
@@ -14,8 +14,10 @@ def use_model():
     mnist = input_data.read_data_sets("/tmp/data/", one_hot=True)
 
     # tf Graph Input
-    x = tf.get_variable("input_image", shape=[10,784], dtype=tf.float32)
-    y = tf.placeholder(shape=[10,10], name='input_label', dtype=tf.float32)  # 0-9 digits recognition => 10 classes
+    grads = tf.placeholder(tf.float32, shape=[100, 784])
+    
+    x = tf.get_variable("input_image", shape=[100,784], dtype=tf.float32)
+    y = tf.placeholder(shape=[100,10], name='input_label', dtype=tf.float32)  # 0-9 digits recognition => 10 classes
 
     # set model weights
     W = tf.get_variable("weights", shape=[784, 10], dtype=tf.float32, initializer=tf.random_normal_initializer())
@@ -27,6 +29,7 @@ def use_model():
 
     # minimize error using cross entropy
     cost = tf.reduce_mean(-tf.reduce_sum(y * tf.log(pred), reduction_indices=1))
+    
     # Gradient Descent
     optimizer = tf.train.GradientDescentOptimizer(FLAGS.learning_rate).minimize(cost)
 
@@ -56,21 +59,56 @@ def use_model():
                 continue
 
             # if there are 10 images stored then end the loop
-            if len(labels_of_2) == 10:
+            if len(labels_of_2) == 100:
                 break
 
+        
         # convert into a numpy array of shape [10, 784]
         labels_of_2 = np.concatenate(labels_of_2, axis=0)
+
 
         # assign the sample image to the variable
         sess.run(tf.assign(x, labels_of_2))
         # setup softmax
         sess.run(pred)
 
+        '''epsilons = np.array([-0.2, -0.4, -0.25, -0.25, 
+                     -0.3, -0.515, -0.15, -0.7, 
+                     -1.0, -0.20]).reshape((10, 1))
+        
         # placeholder for target label
         fake_label = tf.placeholder(tf.int32, shape=[10])
         # setup the fake loss
         fake_loss = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=logits,labels=fake_label)
+
+        gradients = tf.gradients(fake_loss, x)
+
+        gradient_value = sess.run(gradients, feed_dict={fake_label:np.array([6]*10)})
+
+        sign = np.sign(gradient_value[0][0])
+
+        noise = epsilons * sign;
+
+        labels_of_2[0] = labels_of_2[0] + noise[0]
+
+        #print(labels_of_2[0])
+
+        # assign the sample image to the variable
+        sess.run(tf.assign(x, labels_of_2))
+        # setup softmax
+        sess.run(pred)'''
+
+        '''for i in range(FLAGS.training_epochs):
+            gradient_value = sess.run(gradients, feed_dict={fake_label:np.array([6]*10)})
+            gradient_update = x+(0.007*gradient_value[0])
+            sess.run(tf.assign(x, gradient_update))
+            sess.run(pred)'''
+
+        '''
+        
+        
+
+        #sess.run(x.eval())
 
         # minimize fake loss using gradient descent,
         # calculating the derivatives of the weight of the fake image will give the direction of weights necessary to change the prediction
@@ -80,18 +118,19 @@ def use_model():
         for i in range(FLAGS.training_epochs):
             # fake label tells the training algorithm to use the weights calculated for number 6
             sess.run(adversarial_step, feed_dict={fake_label:np.array([6]*10)})
-            sess.run(pred)
+            
+            sess.run(pred)'''
 
         plt.subplot(2,2,1)
-        plt.imshow(sess.run(x[0]).reshape(28,28),cmap='gray')
+        plt.imshow(sess.run(x[4]).reshape(28,28),cmap='gray')
 
         
         plt.show()
 
       
-        x_in = np.expand_dims(x[0], axis=0)
+        x_in = np.expand_dims(x[90], axis=0)
         classification = sess.run(tf.argmax(pred, 1))
         print(classification)
-
+        
 use_model()
 
